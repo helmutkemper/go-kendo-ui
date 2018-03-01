0aca53e445deee364d6c741626c73889c7f6bcb6
Helmut Kemper <helmut.kemper@gmail.com>
2018-02-28 22:05:36 -0300

    trabalhando

--- KendoUiCalendar_test.go
+++ KendoUiCalendar_test.go
@@ -29,8 +29,8 @@ func ExampleKendoUiCalendar_ToTelerikTemplate() {
     Culture: "en-US",
     Value: time.Now(),
     Dates: []time.Time{
-      time.Date(2018, 2, 28, 21, 12, 0, 0, time.UTC),
-      time.Date(2019, 2, 28, 21, 12, 0, 0, time.UTC),
+      time.Date(2018, 2, 28, 0, 0, 0, 0, time.UTC),
+      time.Date(2019, 2, 28, 23, 59, 0, 0, time.UTC),
     },
   }
   javaScript.ToJavaScript()
